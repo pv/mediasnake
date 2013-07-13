f48ef3f3e2bf131266c66bf912bdf9bd3ec9afc5
Pauli Virtanen <pav@iki.fi>
2013-07-13 03:38:02 +0300

    Add bootstrap script + fix requirements

---
+++ bootstrap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+def run(*a, **kw):
+    ret = subprocess.call(*a, **kw)
+    if ret != 0:
+        print("Error runnning: %r" % a)
+        raise SystemExit(1)
+
+def main():
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'mediasnake.settings.production'
+    run(['virtualenv', 'env'])
+    print("\n\n" + "-"*79)
+    print("Downloading and installing requirements")
+    print("\n\n")
+    run(['./env/bin/pip', 'install', '-r', 'requirements.txt'])
+    print("\n\n" + "-"*79)
+    print("Answer 'yes' to whether you want to create a superuser")
+    print("\n\n")
+    run(['./env/bin/python', 'manage.py', 'syncdb'])
+    run(['./env/bin/python', 'manage.py', 'migrate'])
+    print("\n\n" + "-"*79)
+    print("Answer 'yes' to the question")
+    print("\n\n")
+    run(['./env/bin/python', 'manage.py', 'collectstatic'])
+    print("\n\n" + "-"*79)
+    print("Database + environment setup done!")
+    print("\n\n")
+
+if __name__ == "__main__":
+    main()
